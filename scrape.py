422fc7146628a7fa2a85ab33a0fc643df62c5e9d
Nevian85 <54417440+Nevian85@users.noreply.github.com>
2019-11-07 11:01:30 -0800

    Print URL to troubleshoot issue/fixed GSURLPID
    PID was coming up false on PS4 titles. Fixed with IF statement.
    
    GS is now blocking header type. Need to find way around.

--- scrape.py
+++ scrape.py
@@ -15,10 +15,13 @@ print("")
 print("Press enter to exit")
 print("")
 
+
+
 def loadSoup(target_url):
 	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:59.0) Gecko/20100101 Firefox/59.0"}
 	response = requests.get(target_url, headers=headers)
 	html = response.content
+
 	return BeautifulSoup(html, features="html.parser")
 
 def scanAndScrape():
@@ -27,9 +30,6 @@ def scanAndScrape():
 
 	if query == "":
 		return
-
-	outfile = open("./games.csv", "a", newline='')
-	writer = csv.writer(outfile)
 
 	pc_url = 'https://www.pricecharting.com/search-products?type=videogames&q='+ str(query)
 	pc_soup = loadSoup(pc_url)
@@ -47,12 +47,16 @@ def scanAndScrape():
 
 		if gs_url is not None:
 			gs_pid = gs_url["href"].split("%2F")[-1]
-			formatted_gs_pid = f'1{gs_pid:0>7}'
-
+			if len(gs_pid) is 8:
+				formatted_gs_pid = gs_pid
+			else:
+				formatted_gs_pid = f'1{gs_pid:0>7}'
 
 		if gs_url is not None and len(formatted_gs_pid) is 8:
 			gs_trade_url = 'https://www.gamestop.com/trade/details/?pid='+ str(formatted_gs_pid)
 			gs_soup = loadSoup(gs_trade_url)
+			print(gs_soup)
+			print(gs_trade_url)
 			gs_name = gs_soup.find('h1', attrs={'class': 'product-name h2'}).text.strip()
 			gs_trade = gs_soup.select("#trade-values-1 > div > div.grid-i1.col-sm-6.col-12 > ul > li:nth-of-type(2) > div.pull-right > span")
 			gs_trade = gs_trade[0].text.strip()
@@ -64,13 +68,16 @@ def scanAndScrape():
 		print(title,platform,query,genre,loose_price,cib_price,nib_price,gs_name,formatted_gs_pid,gs_trade)
 		print("")
 		GameData = ["=\"" + query + "\"",title,platform,genre,loose_price,cib_price,nib_price,gs_name,formatted_gs_pid,gs_trade]
+
+		outfile = open("./games.csv", "a", newline='')
+		writer = csv.writer(outfile)
 		writer.writerow(GameData)
 	else:
 		print("Unknown UPC")
-		print("")
+		outfile = open("./games.csv", "a", newline='')
+		writer = csv.writer(outfile)
 		writer.writerow(["Uknown UPC"])
-	
-	outfile.flush()
 	scanAndScrape()
 
+# Initializes the script
 scanAndScrape()
